f878a28cbc78506a8438c5f9351aa787c0285f29
funnyguy26062018 <sebastianlinn88@gmail.com>
2026-02-15 18:21:39 +0100

    Create output.json in workflow

--- ticketing.py
+++ ticketing.py
@@ -201,12 +201,9 @@ def getParsedHTML(session, url):
     return html
 
 def saveInGoogleSheets(tickets):
-    payload = {
-        "somekey": "somevalue",
-        "source": "pythonanywhere"
-    }
+    payload = json.dumps(tickets, ensure_ascii=False, indent=2)
     try:
-        r = requests.post(APPS_SCRIPT_URL, json=tickets, timeout=20)
+        r = requests.post(APPS_SCRIPT_URL, json=payload, timeout=20)
         print("STATUS CODE:", r.status_code)
         print("RESPONSE TEXT:", r.text)
         print("Apps Script response:", r.text)
